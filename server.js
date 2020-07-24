849065e9752ed585736141a1ac768c0c15a4b4c4
Shreyash <shreyashghadge11@gmail.com>
2020-07-24 15:59:17 +0530

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 const path = require("path");
 
