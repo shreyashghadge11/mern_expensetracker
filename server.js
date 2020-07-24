53e07929e0dbfa8c6bd0bb2597de4e0dffaaa077
Shreyash <shreyashghadge11@gmail.com>
2020-07-24 19:26:53 +0530

    changes

--- server.js
+++ server.js
@@ -30,14 +30,12 @@ mongoose.Promise = global.Promise;
 
 app.use("/api", require("./routes/route"));
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use("/", express.static(path.join(__dirname, "/client/build")));
+// Set static folder
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`server up and running on ${port}`);
