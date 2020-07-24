087ddb41274dfecc7648b8a88fdfc21321b78e1a
Shreyash <shreyashghadge11@gmail.com>
2020-07-24 18:42:24 +0530

    changes

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.use("/api", require("./routes/route"));
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
